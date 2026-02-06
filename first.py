46ced66a33df0ba23a62cd2dd5c2f57d01e060ac
strveixk-goat <lee.stephen8282@gmail.com>
2026-02-06 11:48:07 -0500

    I made a program

--- first.py
+++ first.py
@@ -1,2 +1,7 @@
-# This is a comment, the interpreter ignores this line
-print("Sup cuh?")
+# Legit program
+# num1 = 6
+# num2 = 7
+# print(6+7)
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+print(num1+num2)
